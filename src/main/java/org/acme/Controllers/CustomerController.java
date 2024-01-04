788b6ae2b96981db348cd4581fbae103c2ff7f0a
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2024-01-04 16:10:16 +0100

    SOAP task 1 pending

--- src/main/java/org/acme/Controllers/CustomerController.java
+++ src/main/java/org/acme/Controllers/CustomerController.java
@@ -1,5 +1,21 @@
 package org.acme.Controllers;
 
+import org.acme.Services.CustomersService;
+import org.jboss.logging.annotations.Producer;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+
+
 public class CustomerController {
-    
+    CustomersService customersService = new CustomersService();
+    @Path("/customer/register")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    private void register(){
+        
+    }
+
 }
